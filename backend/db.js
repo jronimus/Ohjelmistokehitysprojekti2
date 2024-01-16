c52c7a5fdeb7d162cca2b428c35fd85eafed03a0
Aresla <areslas@list.ru>
2024-01-16 14:57:40 +0200

    add database connection and index

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error);
+  }
+};
+
+module.exports = connectDB;
