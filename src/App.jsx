7d0433c68fd00e23e0378776e21159f0bd57664f
mikathefinn <mikathefinn@gmail.com>
2024-01-19 09:13:47 +0200

    pin to the correct folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import NavBar from './components/NavBar'
 import GoogleMaps from "./components/GoogleMaps"
  import Weather from './components/WeatherForecast/Weather'
+ import Datahub from './components/Datahub'
 // import Weather2 from './components/Weather2'
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
        <NavBar setCity={setCity}/>
       <GoogleMaps/>
       <Weather city={city} />
-     
+     <Datahub />
     </>
   )
 }
--- src/components/GoogleMaps.jsx
+++ src/components/GoogleMaps.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import pin from '../../public/pin.png'
+import pin from '../Images/Icons/pin.png'
 import mapOptions from './mapSkin'
 
 import {
